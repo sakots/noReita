44b972b2f6a58084c8512d8609d870aa6e900ebe
sakots <sakots8000@gmail.com>
2021-11-30 11:55:49 +0900

    theme monor

---
+++ noreita/theme/monor/headcss.blade.php
@@ -0,0 +1,4 @@
+@section('headcss')
+        <link rel="stylesheet" href="theme/{{$themedir}}/css/monored_index.min.css">
+		<link rel="stylesheet" href="theme/{{$themedir}}/css/main/mono.min.css">
+@show
