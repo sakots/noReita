49d99cb5e9f8d801867429f9833d5c589e5393b9
sakots <sakots8000@gmail.com>
2021-12-06 13:13:34 +0900

    theme monoreita

--- noreita/theme/monoreita/fix_chiken/fix.js
+++ noreita/theme/monoreita/fix_chiken/fix.js
@@ -1,4 +1,8 @@
-function load() {
-    document.addEventListener("dblclick", function(e){ e.preventDefault()}, { passive: false });
-}
-window.onload = load;
+function fixchicken() {
+    document.addEventListener('dblclick', function(e){ e.preventDefault()}, { passive: false });
+    document.querySelector('#chickenpaint-parent').addEventListener('contextmenu', function (e){
+        e.preventDefault();
+        e.stopPropagation();
+    }, { passive: false });
+    }
+window.addEventListener('DOMContentLoaded',fixchicken,false);
