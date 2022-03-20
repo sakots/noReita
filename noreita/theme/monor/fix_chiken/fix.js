77d1e2d33c642b053ceefb3c1f7e456b56e2b92a
sakots <sakots8000@gmail.com>
2022-03-21 00:34:25 +0900

    theme monor

--- noreita/theme/monor/fix_chiken/fix.js
+++ noreita/theme/monor/fix_chiken/fix.js
@@ -1,8 +1,9 @@
 function fixchicken() {
     document.addEventListener('dblclick', function(e){ e.preventDefault()}, { passive: false });
-    document.querySelector('#chickenpaint-parent').addEventListener('contextmenu', function (e){
+    const chicken=document.querySelector('#chickenpaint-parent');
+    chicken.addEventListener('contextmenu', function (e){
         e.preventDefault();
         e.stopPropagation();
     }, { passive: false });
-    }
+}
 window.addEventListener('DOMContentLoaded',fixchicken,false);
