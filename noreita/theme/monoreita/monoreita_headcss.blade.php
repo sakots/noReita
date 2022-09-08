869c50d66486407fa26cec18967d34ebf15630e2
sakots <sakots8000@gmail.com>
2022-09-08 22:10:32 +0900

    monoreita

--- noreita/theme/monoreita/monoreita_headcss.blade.php
+++ noreita/theme/monoreita/monoreita_headcss.blade.php
@@ -1,13 +1,13 @@
 @section('headcss')
-        <link rel="stylesheet" href="theme/{{$themedir}}/css/monored_index.min.css">
-		<link rel="stylesheet" href="theme/{{$themedir}}/css/reita/mono.min.css">
-		<link rel="stylesheet" href="theme/{{$themedir}}/css/red/mono.min.css" id="css1" disabled>
-		<link rel="stylesheet" href="theme/{{$themedir}}/css/main/mono.min.css" id="css2" disabled>
-		<link rel="stylesheet" href="theme/{{$themedir}}/css/dark/mono.min.css" id="css3" disabled>
-		<link rel="stylesheet" href="theme/{{$themedir}}/css/deep/mono.min.css" id="css4" disabled>
-		<link rel="stylesheet" href="theme/{{$themedir}}/css/mayo/mono.min.css" id="css5" disabled>
-		<link rel="stylesheet" href="theme/{{$themedir}}/css/dev/mono.min.css" id="css6" disabled>
-		<link rel="stylesheet" href="theme/{{$themedir}}/css/sql/mono.min.css" id="css7" disabled>
-		<link rel="stylesheet" href="theme/{{$themedir}}/css/pop/mono.min.css" id="css8" disabled>
-		<script src="theme/{{$themedir}}/js/switchcss.js"></script>
+<link rel="stylesheet" href="theme/{{$themedir}}/css/monored_index.min.css">
+<link rel="stylesheet" href="theme/{{$themedir}}/css/reita/mono.min.css">
+<link rel="stylesheet" href="theme/{{$themedir}}/css/red/mono.min.css" id="css1" disabled>
+<link rel="stylesheet" href="theme/{{$themedir}}/css/main/mono.min.css" id="css2" disabled>
+<link rel="stylesheet" href="theme/{{$themedir}}/css/dark/mono.min.css" id="css3" disabled>
+<link rel="stylesheet" href="theme/{{$themedir}}/css/deep/mono.min.css" id="css4" disabled>
+<link rel="stylesheet" href="theme/{{$themedir}}/css/mayo/mono.min.css" id="css5" disabled>
+<link rel="stylesheet" href="theme/{{$themedir}}/css/dev/mono.min.css" id="css6" disabled>
+<link rel="stylesheet" href="theme/{{$themedir}}/css/sql/mono.min.css" id="css7" disabled>
+<link rel="stylesheet" href="theme/{{$themedir}}/css/pop/mono.min.css" id="css8" disabled>
+<script src="theme/{{$themedir}}/js/switchcss.js"></script>
 @show
