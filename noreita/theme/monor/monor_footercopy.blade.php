bcc5d8a7543349879be3a68977ac26b5e755b2d1
sakots <sakots8000@gmail.com>
2025-06-11 15:16:20 +0900

    copyright

--- noreita/theme/monor/monor_footercopy.blade.php
+++ noreita/theme/monor/monor_footercopy.blade.php
@@ -7,13 +7,16 @@
 	</p>
 	<p>
 		OekakiApplet -
+		@if ($use_shi_painter)
+		<!-- https://hp.vector.co.jp/authors/VA016309/ -->
+		<span title="by しぃちゃん">Shi-Painter, </span>
+		@endif
 		<a href="https://github.com/funige/neo/" target="_blank" rel="noopener noreferrer" title="by funige">PaintBBS NEO</a>
 		@if ($use_chicken) ,<a href="https://github.com/satopian/ChickenPaint_Be" target="_blank" rel="nofollow noopener noreferrer" title="by Nicholas Sherlock">ChickenPaint Be</a> @endif
 	</p>
 	<p>
 		UseFunction -
 		<!-- http://wondercatstudio.com/ -->DynamicPalette,
-		<a href="https://huruihone.tumblr.com/" target="_blank" rel="noopener noreferrer" title="by Soto">AppletFit</a>,
 		<a href="https://github.com/imgix/luminous" target="_blank" rel="noopener noreferrer" title="by imgix">Luminous</a>,
 		<a href="https://github.com/EFTEC/BladeOne" target="_blank" rel="noopener noreferrer" title="by EFTEC">BladeOne</a>
 	</p>
