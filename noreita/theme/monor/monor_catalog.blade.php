488f3d645fdf4fc5a8813cd7d59655ab43b2d408
sakots <sakots8000@gmail.com>
2026-03-28 22:20:07 +0900

    fix: カタログモードのリンク修正

--- noreita/theme/monor/monor_catalog.blade.php
+++ noreita/theme/monor/monor_catalog.blade.php
@@ -150,19 +150,19 @@
 						@if ($back === 0)
 						<span class="se">[START]</span>
 						@else
-						<span class="se">&lt;&lt;<a href="{{$self}}?page={{$back}}">[BACK]</a></span>
+						<span class="se">&lt;&lt;<a href="{{$self}}?mode=catalog&amp;page={{$back}}">[BACK]</a></span>
 						@endif
 						@foreach ($paging as $pp)
 						@if ($pp['p'] == $nowpage)
 						<em class="thispage">[{{$pp['p']}}]</em>
 						@else
-						<a href="{{$self}}?page={{$pp['p']}}">[{{$pp['p']}}]</a>
+						<a href="{{$self}}?mode=catalog&amp;page={{$pp['p']}}">[{{$pp['p']}}]</a>
 						@endif
 						@endforeach
 						@if ($next == ($max_page + 1))
 						<span class="se">[END]</span>
 						@else
-						<span class="se"><a href="{{$self}}?page={{$next}}">[NEXT]</a>&gt;&gt;</span>
+						<span class="se"><a href="{{$self}}?mode=catalog&amp;page={{$next}}">[NEXT]</a>&gt;&gt;</span>
 						@endif
 					</p>
 				</section>
