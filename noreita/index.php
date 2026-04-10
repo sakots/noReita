bef460806563a3521eb7474ac354d7af797369f5
sakots <sakots8000@gmail.com>
2026-04-11 06:32:45 +0900

    v3.0.3

--- noreita/index.php
+++ noreita/index.php
@@ -5,7 +5,7 @@
 //--------------------------------------------------
 
 //スクリプトのバージョン
-define('REITA_VER', 'v3.0.2'); //lot.2604010.1
+define('REITA_VER', 'v3.0.3'); //lot.2604011.0
 
 //言語判定
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
