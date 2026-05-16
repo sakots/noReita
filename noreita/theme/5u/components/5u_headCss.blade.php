9ad7cadce0ec562d7d82e753e2daafe23bde465e
sakots <sakots8000@gmail.com>
2026-05-16 21:47:22 +0900

    cssテスト

--- noreita/theme/5u/components/5u_headCss.blade.php
+++ noreita/theme/5u/components/5u_headCss.blade.php
@@ -1,13 +1,5 @@
 @section('headCss')
 <link rel="stylesheet" href="theme/{{$theme_dir}}/css/5u_main.min.css">
 <link rel="stylesheet" href="theme/{{$theme_dir}}/css/plane/5u.min.css" id="css1">
-<link rel="stylesheet" href="theme/{{$theme_dir}}/css/red/5u.min.css" id="css2" disabled>
-<link rel="stylesheet" href="theme/{{$theme_dir}}/css/main/5u.min.css" id="css3" disabled>
-<link rel="stylesheet" href="theme/{{$theme_dir}}/css/dark/5u.min.css" id="css4" disabled>
-<link rel="stylesheet" href="theme/{{$theme_dir}}/css/deep/5u.min.css" id="css5" disabled>
-<link rel="stylesheet" href="theme/{{$theme_dir}}/css/mayo/5u.min.css" id="css6" disabled>
-<link rel="stylesheet" href="theme/{{$theme_dir}}/css/dev/5u.min.css" id="css7" disabled>
-<link rel="stylesheet" href="theme/{{$theme_dir}}/css/sql/5u.min.css" id="css8" disabled>
-<link rel="stylesheet" href="theme/{{$theme_dir}}/css/pop/5u.min.css" id="css9" disabled>
 <script src="theme/{{$theme_dir}}/js/switchcss.js"></script>
 @show
