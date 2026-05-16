2c2ce6c354da5e432e64732cbe26722eb37f0494
sakots <sakots8000@gmail.com>
2026-05-16 21:35:50 +0900

    cssテスト

--- noreita/theme/5u/components/5u_deleteForm.blade.php
+++ noreita/theme/5u/components/5u_deleteForm.blade.php
@@ -10,15 +10,7 @@
     <label for="mystyle">Color</label>
     <span class="stylechanger">
       <select class="form" name="select" id="mystyle" onchange="SetCss(this);">
-        <option value="reita/mono.min.css">REITA</option>
-        <option value="red/mono.min.css">RED</option>
-        <option value="main/mono.min.css">MONO</option>
-        <option value="dark/mono.min.css">dark</option>
-        <option value="deep/mono.min.css">deep</option>
-        <option value="mayo/mono.min.css">MAYO</option>
-        <option value="dev/mono.min.css">DEV</option>
-        <option value="sql/mono.min.css">SQL</option>
-        <option value="pop/mono.min.css">POP</option>
+        <option value="plane/5u.min.css">plane</option>
       </select>
     </span>
   </p>
