89926ed608a704eb8e5b8abb442713d54e3c0a0a
sakots <sakots8000@gmail.com>
2026-05-17 15:16:56 +0900

    css名変更

--- noreita/theme/monoreita/components/monoreita_headCss.blade.php
+++ noreita/theme/monoreita/components/monoreita_headCss.blade.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="theme/{{$theme_dir}}/css/monoreita_index.min.css">
 <link rel="stylesheet" href="theme/{{$theme_dir}}/css/reita/monoreita.min.css" id="css0">
 <link rel="stylesheet" href="theme/{{$theme_dir}}/css/red/monoreita.min.css" id="css1" disabled>
-<link rel="stylesheet" href="theme/{{$theme_dir}}/css/main/monoreita.min.css" id="css2" disabled>
+<link rel="stylesheet" href="theme/{{$theme_dir}}/css/mono/monoreita.min.css" id="css2" disabled>
 <link rel="stylesheet" href="theme/{{$theme_dir}}/css/dark/monoreita.min.css" id="css3" disabled>
 <link rel="stylesheet" href="theme/{{$theme_dir}}/css/deep/monoreita.min.css" id="css4" disabled>
 <link rel="stylesheet" href="theme/{{$theme_dir}}/css/mayo/monoreita.min.css" id="css5" disabled>
