b7e9b63652efdb6d0c36738e301d17ffa28b3adb
sakots <sakots8000@gmail.com>
2026-05-17 14:57:47 +0900

    fix: css番号がズレていたの修正

--- noreita/theme/monoreita/components/monoreita_headCss.blade.php
+++ noreita/theme/monoreita/components/monoreita_headCss.blade.php
@@ -1,11 +1,11 @@
 <link rel="stylesheet" href="theme/{{$theme_dir}}/css/monoreita_index.min.css">
-<link rel="stylesheet" href="theme/{{$theme_dir}}/css/reita/mono.min.css" id="css1">
-<link rel="stylesheet" href="theme/{{$theme_dir}}/css/red/mono.min.css" id="css2" disabled>
-<link rel="stylesheet" href="theme/{{$theme_dir}}/css/main/mono.min.css" id="css3" disabled>
-<link rel="stylesheet" href="theme/{{$theme_dir}}/css/dark/mono.min.css" id="css4" disabled>
-<link rel="stylesheet" href="theme/{{$theme_dir}}/css/deep/mono.min.css" id="css5" disabled>
-<link rel="stylesheet" href="theme/{{$theme_dir}}/css/mayo/mono.min.css" id="css6" disabled>
-<link rel="stylesheet" href="theme/{{$theme_dir}}/css/dev/mono.min.css" id="css7" disabled>
-<link rel="stylesheet" href="theme/{{$theme_dir}}/css/sql/mono.min.css" id="css8" disabled>
-<link rel="stylesheet" href="theme/{{$theme_dir}}/css/pop/mono.min.css" id="css9" disabled>
+<link rel="stylesheet" href="theme/{{$theme_dir}}/css/reita/mono.min.css" id="css0">
+<link rel="stylesheet" href="theme/{{$theme_dir}}/css/red/mono.min.css" id="css1" disabled>
+<link rel="stylesheet" href="theme/{{$theme_dir}}/css/main/mono.min.css" id="css2" disabled>
+<link rel="stylesheet" href="theme/{{$theme_dir}}/css/dark/mono.min.css" id="css3" disabled>
+<link rel="stylesheet" href="theme/{{$theme_dir}}/css/deep/mono.min.css" id="css4" disabled>
+<link rel="stylesheet" href="theme/{{$theme_dir}}/css/mayo/mono.min.css" id="css5" disabled>
+<link rel="stylesheet" href="theme/{{$theme_dir}}/css/dev/mono.min.css" id="css6" disabled>
+<link rel="stylesheet" href="theme/{{$theme_dir}}/css/sql/mono.min.css" id="css7" disabled>
+<link rel="stylesheet" href="theme/{{$theme_dir}}/css/pop/mono.min.css" id="css8" disabled>
 <script src="theme/{{$theme_dir}}/js/switchcss.js"></script>
