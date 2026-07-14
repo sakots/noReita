5564df965d9a0f9a99f971bddb1fca3ac0cf7ff5
sakots <sakots8000@gmail.com>
2026-07-14 13:32:46 +0900

    fix: 手ブレの初期値を0に

--- noreita/theme/monoreita/components/monoreita_dynamicPalette.blade.php
+++ noreita/theme/monoreita/components/monoreita_dynamicPalette.blade.php
@@ -14,8 +14,8 @@
       <input class="button" type="button" value="右" onclick="Neo.setToolSide(false)">
       手ぶれ
       <select onchange="Neo.setStabilizeLevel(this.value)">
-        <option value="0">0</option>
-        <option value="1" selected>1</option>
+        <option value="0" selected>0</option>
+        <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
         <option value="4">4</option>
