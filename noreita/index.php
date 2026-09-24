041186ff8d9db2b62d902f620a29ea1b0e7850ba
sakots <sakots8000@gmail.com>
2026-09-24 13:37:04 +0900

    v4.12.2

--- noreita/index.php
+++ noreita/index.php
@@ -5,7 +5,7 @@
 //--------------------------------------------------
 
 // スクリプトのバージョン
-const REITA_VER = 'v4.12.1 lot.260922.0';
+const REITA_VER = 'v4.12.2 lot.260924.0';
 
 require_once __DIR__ . '/app_bootstrap.inc.php';
 $en = app_bootstrap(__DIR__);
